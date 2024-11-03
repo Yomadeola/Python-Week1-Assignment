3c892c8e4786d198ff5209247c64ecb8541a18dc
Yomadeola <100537328+Yomadeola@users.noreply.github.com>
2024-11-03 00:51:31 +0000

    Add files via upload

---
+++ user-input.py
@@ -0,0 +1,11 @@
+name = input("What is your name: ")
+print(f"Your name is {name}")
+
+age = input("How old are you: ")
+print(f"You are {age} years old")
+
+location = input("Where do you stay: ")
+print(f"You stay at {location}")
+
+
+print(f"Hello there, This is my first python code. I feel so excited learning python and also getting to know you. Your name is {name} and you are {age} years old and you stay at {location}")
